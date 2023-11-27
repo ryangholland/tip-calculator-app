5dd612a07ac833f13a70fb266b31dcbbdff04583
ryangholland <rghcode@gmail.com>
2023-11-27 17:21:01 -0500

    Allow user to select set tip values

--- src/components/Tip.jsx
+++ src/components/Tip.jsx
@@ -1,23 +1,46 @@
-function Tip() {
+import PropTypes from "prop-types";
+
+function Tip({ tip, setTip }) {
   return (
     <div className="form-group">
       <p>Select Tip %</p>
 
       <div className="radio-group">
         <div>
-          <input type="radio" id="five" name="tip" value="5%" />
+          <input
+            type="radio"
+            id="five"
+            name="tip"
+            value={0.05}
+            checked={tip === 0.05}
+            onChange={() => setTip(0.05)}
+          />
           <label htmlFor="five" className="radio-btn">
             5%
           </label>
         </div>
         <div>
-          <input type="radio" id="ten" name="tip" value="10%" />
+          <input
+            type="radio"
+            id="ten"
+            name="tip"
+            value={0.1}
+            checked={tip === 0.1}
+            onChange={() => setTip(0.1)}
+          />
           <label htmlFor="ten" className="radio-btn">
             10%
           </label>
         </div>
         <div>
-          <input type="radio" id="fifteen" name="tip" value="15%" checked />
+          <input
+            type="radio"
+            id="fifteen"
+            name="tip"
+            value={0.15}
+            checked={tip === 0.15}
+            onChange={() => setTip(0.15)}
+          />
           <label htmlFor="fifteen" className="radio-btn">
             15%
           </label>
@@ -26,13 +49,26 @@ function Tip() {
 
       <div className="radio-group">
         <div>
-          <input type="radio" id="twenty-five" name="tip" value="25%" />
+          <input
+            type="radio"
+            id="twenty-five"
+            value={0.25}
+            checked={tip === 0.25}
+            onChange={() => setTip(0.25)}
+          />
           <label htmlFor="twenty-five" className="radio-btn">
             25%
           </label>
         </div>
         <div>
-          <input type="radio" id="fifty" name="tip" value="50%" />
+          <input
+            type="radio"
+            id="fifty"
+            name="tip"
+            value={0.5}
+            checked={tip === 0.5}
+            onChange={() => setTip(0.5)}
+          />
           <label htmlFor="fifty" className="radio-btn">
             50%
           </label>
@@ -47,5 +83,10 @@ function Tip() {
     </div>
   );
 }
+
+Tip.propTypes = {
+  tip: PropTypes.number,
+  setTip: PropTypes.func,
+};
 
 export default Tip;
