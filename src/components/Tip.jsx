0ade1e6252167b6a48b7b10abae8926c2011a77c
ryangholland <rghcode@gmail.com>
2023-11-27 17:40:21 -0500

    Add custom tip functionality

--- src/components/Tip.jsx
+++ src/components/Tip.jsx
@@ -1,6 +1,10 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 function Tip({ tip, setTip }) {
+  const [changingCustom, setChangingCustom] = useState(false);
+  const [custom, setCustom] = useState(0);
+
   return (
     <div className="form-group">
       <p>Select Tip %</p>
@@ -13,7 +17,11 @@ function Tip({ tip, setTip }) {
             name="tip"
             value={0.05}
             checked={tip === 0.05}
-            onChange={() => setTip(0.05)}
+            onChange={() => {
+              setTip(0.05);
+              setCustom(0);
+              setChangingCustom(false);
+            }}
           />
           <label htmlFor="five" className="radio-btn">
             5%
@@ -26,7 +34,11 @@ function Tip({ tip, setTip }) {
             name="tip"
             value={0.1}
             checked={tip === 0.1}
-            onChange={() => setTip(0.1)}
+            onChange={() => {
+              setTip(0.1);
+              setCustom(0);
+              setChangingCustom(false);
+            }}
           />
           <label htmlFor="ten" className="radio-btn">
             10%
@@ -39,7 +51,11 @@ function Tip({ tip, setTip }) {
             name="tip"
             value={0.15}
             checked={tip === 0.15}
-            onChange={() => setTip(0.15)}
+            onChange={() => {
+              setTip(0.15);
+              setCustom(0);
+              setChangingCustom(false);
+            }}
           />
           <label htmlFor="fifteen" className="radio-btn">
             15%
@@ -54,7 +70,11 @@ function Tip({ tip, setTip }) {
             id="twenty-five"
             value={0.25}
             checked={tip === 0.25}
-            onChange={() => setTip(0.25)}
+            onChange={() => {
+              setTip(0.25);
+              setCustom(0);
+              setChangingCustom(false);
+            }}
           />
           <label htmlFor="twenty-five" className="radio-btn">
             25%
@@ -67,17 +87,53 @@ function Tip({ tip, setTip }) {
             name="tip"
             value={0.5}
             checked={tip === 0.5}
-            onChange={() => setTip(0.5)}
+            onChange={() => {
+              setTip(0.5);
+              setCustom(0);
+              setChangingCustom(false);
+            }}
           />
           <label htmlFor="fifty" className="radio-btn">
             50%
           </label>
         </div>
         <div>
-          <input type="radio" id="custom" name="tip" value="custom" />
-          <label htmlFor="custom" className="radio-btn">
-            Custom
-          </label>
+          {!changingCustom && (
+            <>
+              <input
+                type="radio"
+                id="custom"
+                name="tip"
+                value="custom"
+                checked={changingCustom}
+                onChange={() => {
+                  setTip(0);
+                  setChangingCustom(!changingCustom);
+                }}
+              />
+              <label htmlFor="custom" className="radio-btn">
+                Custom
+              </label>
+            </>
+          )}
+          {changingCustom && (
+            <>
+              <input
+                type="number"
+                id="customAmount"
+                name="customAmount"
+                step="1"
+                min="00"
+                max="100"
+                placeholder="00"
+                value={custom}
+                onChange={(e) => {
+                  setCustom(e.target.value);
+                  setTip(e.target.value * 0.01);
+                }}
+              ></input>
+            </>
+          )}
         </div>
       </div>
     </div>
